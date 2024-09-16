9034152f3cc0c72f8684c2263e47b9afff2d57c1
estrella1245 <emonroy13@student.cccd.edu>
2024-09-15 21:43:44 -0700

    made two statements

--- voting-age.js
+++ voting-age.js
@@ -10,7 +10,9 @@ console.log('age is ' + age);
 
 // TODO: Checks the given age to see whether it's legal to vote in the US or not.
 // Returns true if the age is 18 years or older. Otherwise, returns false.
-const result = (parseInt(age) >= 18) ? result = true : result = false;
+let result = undefined;
+
+(parseInt(age) >= 18) ? result = true : result = false;
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
 console.log('result= ' + result);
